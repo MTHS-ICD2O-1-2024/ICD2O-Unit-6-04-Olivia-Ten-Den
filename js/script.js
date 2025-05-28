c273ee901713f54978af586fdbf572917b355664
Olivia Ten Den <olivia.ten-den@stu.ocsb.ca>
2025-05-28 17:27:41 +0000

    tem

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Olivia Ten Den
+// Created on: May 6, 2025
 // This file contains the JS functions for index.html
 
 "use strict"

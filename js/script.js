0cd98fe2e149d5554dbbe76b5ed98685148765a7
Olivia Ten Den <olivia.ten-den@stu.ocsb.ca>
2025-05-28 17:24:25 +0000

    q

--- js/script.js
+++ js/script.js
@@ -3,3 +3,32 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+  // this calculates volume of a pyramid
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const aBase = params.get("aBase")
+  const bBase = params.get("bBase")
+  const height = params.get("height")
+  console.log(aBase)
+
+  // process
+  const area = ((aBase + bBase) / 2) * height
+  const dimensions =
+    "<ul>\n<li>a base = " +
+    aBase +
+    "</li>\n<li>b base = " +
+    bBase +
+    "</li>\n<li>height = " +
+    height +
+    "</li>\n</ul>"
+
+  // output
+  document.getElementById("dimensions").innerHTML = dimensions
+  document.getElementById("area").innerHTML = "Area is: " + area + " mm²"
+}
